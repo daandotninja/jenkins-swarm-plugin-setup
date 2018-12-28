04a5671855e193781b868f1596199b4d9a1b6bdf
Daan  Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2018-12-28 03:31:06 +0100

    config.sh sprict

--- swarm-client.sh
+++ swarm-client.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source ./config.sh
+source /usr/local/bin/swarmclient/config.sh
 
 sudo java -jar /usr/local/bin/swarmclient/swarm-client-$swarmversion.jar -master $master -username $username -password $password  -disableClientsUniqueId
 
