78596bf6ac7a3e9cca5fd9c08eaa66987ee821a5
Daan  Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2018-12-28 03:24:01 +0100

    user

--- jenkins-slave-setup.sh
+++ jenkins-slave-setup.sh
@@ -4,18 +4,18 @@ source ./config.sh
 
 
 
-mkdir /usr/local/bin/swarmclient;
+mkdir /var/swarmclient;
 
-curl  -O http://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/$swarmversion/swarm-client-$swarmversion.jar > /usr/local/bin/swarmclient/swarm-client-$swarmversion.jar
+curl  -O http://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/$swarmversion/swarm-client-$swarmversion.jar > /var/swarmclient/swarm-client-$swarmversion.jar
 
 
-sudo cp ./config.sh  /usr/local/bin/swarmclient/config.sh
+sudo cp ./config.sh  /var/swarmclient/config.sh
 
 sudo cp ./swarm-client.service /etc/systemd/system/swarm-client.service
 
-sudo cp ./swarm-client.sh /usr/local/bin/swarmclient/swarm-client.sh
+sudo cp ./swarm-client.sh /var/swarmclient/swarm-client.sh
 
-sudo chmod u+x /usr/local/bin/swarmclient/swarm-client.sh
+sudo chmod u+x /var/swarmclient/swarm-client.sh
 
 sudo systemctl daemon-reload
 sudo systemctl enable swarm-client.service
