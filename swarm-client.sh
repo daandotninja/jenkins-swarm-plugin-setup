b446562f6b0188742c69d2600836e7c7e6bcecf5
Daan  Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2018-12-28 10:51:06 +0100

    rm sudo

--- swarm-client.sh
+++ swarm-client.sh
@@ -2,7 +2,7 @@
 
 source /usr/local/bin/swarmclient/config.sh
 
-sudo java -jar /usr/local/bin/swarmclient/swarm-client-"$swarmversion".jar -master "$master" -username "$username" -password "$password"  -disableClientsUniqueId
+java -jar /usr/local/bin/swarmclient/swarm-client-"$swarmversion".jar -master "$master" -username "$username" -password "$password"  -disableClientsUniqueId
 
 
 
