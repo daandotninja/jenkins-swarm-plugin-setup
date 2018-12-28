04d6a4ed1854c2374991cf189767b8f3c86bdf29
Daan  Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2018-12-28 03:16:12 +0100

    service

--- jenkins-slave-setup.sh
+++ jenkins-slave-setup.sh
@@ -4,21 +4,21 @@ source ./config.sh
 
 
 
-mkdir ~/swarmclient;
+mkdir /var/swarmclient;
 
-curl  -O http://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/$swarmversion/swarm-client-$swarmversion.jar > ~/swarmclient/swarm-client-$swarmversion.jar
+curl  -O http://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/$swarmversion/swarm-client-$swarmversion.jar > /var/swarmclient/swarm-client-$swarmversion.jar
 
 
-sudo cp ./config.sh  ~/swarmclient/config.sh
+sudo cp ./config.sh  /var/swarmclient/config.sh
 
 sudo cp ./swarm-client.service /etc/systemd/system/swarm-client.service
 
-sudo cp ./swarm-client.sh ~/swarmclient/swarm-client.sh
+sudo cp ./swarm-client.sh /var/swarmclient/swarm-client.sh
 
-sudo chmod u+x ~/swarmclient/swarm-client.sh
+sudo chmod u+x /var/swarmclient/swarm-client.sh
 
 sudo systemctl daemon-reload
 sudo systemctl enable swarm-client.service
-sudo systemctl start swarm-clien
+sudo systemctl start swarm-client
 sudo systemctl status swarm-client
 sudo journalctl --unit=swarm-client
--- swarm-client.sh
+++ swarm-client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source ./config.sh
 
-sudo java -jar ~/swarmclient/swarm-client-$swarmversion.jar -master $master -username $username -password $password  -disableClientsUniqueId
+sudo java -jar /var/swarmclient/swarm-client-$swarmversion.jar -master $master -username $username -password $password  -disableClientsUniqueId
 
 
 
